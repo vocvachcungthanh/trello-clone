a5a4072e4904b1c40a4b59282ba58e06c7471a0b
Thành Nguyễn Hữu <thanhnguyenhuu@Thanhs-MacBook-Pro.local>
2023-12-24 23:14:25 +0700

    fetch: config_ui_theme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -42,7 +42,7 @@ module.exports = {
     "linebreak-style": 0,
     "no-unexpected-multiline": "warn",
     "keyword-spacing": 1,
-    "comma-dangle": 1,
+    "comma-dangle": 0,
     "comma-spacing": 1,
     "arrow-spacing": 1,
   },
