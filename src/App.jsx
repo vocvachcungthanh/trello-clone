c63c7d2197a0d5cdf57d8fd4824c6cd8deb7cf2e
Thành Nguyễn Hữu <thanhnguyenhuu@Thanhs-MacBook-Pro.local>
2024-01-10 20:14:16 +0700

    fetch: dark_light_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Button from "@mui/material/Button";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
@@ -6,9 +5,78 @@ import HomeIcon from "@mui/icons-material/Home";
 import { pink } from "@mui/material/colors";
 import Typography from "@mui/material/Typography";
 
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarktModeIcon from "@mui/icons-material/DarkMode";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+import { useColorScheme } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectMode = event.target.value;
+
+    setMode(selectMode);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="lable-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="lable-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <DarktModeIcon fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
+
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
+      <ModeToggle />
       <div>VVCT</div>
       <Typography variant="body2" color="text.secondary">
         Test Typ
