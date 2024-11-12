10543b9156e3c85d6095c09f99bfc57b73373297
cafedev93 <cafdev93@cafedev93s-MacBook-Pro.local>
2024-11-12 23:32:11 +0700

    react-router-and-react-hook

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,32 +1,35 @@
 // TrungQuanDev: https://youtube.com/@trungquandev
-import { useLocation } from 'react-router-dom'
-import Box from '@mui/material/Box'
-import LoginForm from './LoginForm'
-import RegisterForm from './RegisterForm'
+import { useLocation } from "react-router-dom";
+import Box from "@mui/material/Box";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
 
 function Auth() {
-  const location = useLocation()
-  // console.log(location)
-  const isLogin = location.pathname === '/login'
-  const isRegister = location.pathname === '/register'
+  const location = useLocation();
+  // console.log("🚀 ~ Auth ~ location:", location);
+
+  const isLogin = location.pathname === "/login";
+  const isRegister = location.pathname === "/register";
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      background: 'url("src/assets/auth/login-register-bg.jpg")',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      boxShadow: 'inset 0 0 0 2000px rgba(0, 0, 0, 0.2)'
-    }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        background: 'url("src/assets/auth/login-register-bg.jpg")',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.2)",
+      }}
+    >
       {isLogin && <LoginForm />}
       {isRegister && <RegisterForm />}
     </Box>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
--- src/pages/Auth/RegisterForm.jsx
+++ src/pages/Auth/RegisterForm.jsx
@@ -1,63 +1,125 @@
 // TrungQuanDev: https://youtube.com/@trungquandev
-import { Link } from 'react-router-dom'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Avatar from '@mui/material/Avatar'
-import LockIcon from '@mui/icons-material/Lock'
-import Typography from '@mui/material/Typography'
-import { Card as MuiCard } from '@mui/material'
-import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
-import CardActions from '@mui/material/CardActions'
-import TextField from '@mui/material/TextField'
-import Zoom from '@mui/material/Zoom'
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import LockIcon from "@mui/icons-material/Lock";
+import Typography from "@mui/material/Typography";
+import { Card as MuiCard } from "@mui/material";
+import { ReactComponent as TrelloIcon } from "~/assets/trello.svg";
+import CardActions from "@mui/material/CardActions";
+import TextField from "@mui/material/TextField";
+import Zoom from "@mui/material/Zoom";
+import { useForm } from "react-hook-form";
+import {
+  FIELD_REQUIRED_MESSAGE,
+  EMAIL_RULE,
+  EMAIL_RULE_MESSAGE,
+  PASSWORD_RULE,
+  PASSWORD_RULE_MESSAGE,
+  PASSWORD_CONFIRMATION_MESSAGE,
+} from "~/utils/validators";
+
+import FieldErrorAlert from "~/components/Form/FieldErrorAlert";
 
 function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const submitRegister = (data) => {
+    console.log("🚀 ~ submitRegister ~ data:", data);
+  };
   return (
-    // <form onSubmit={handleSubmit(submitRegister)}>
-    <form>
-      <Zoom in={true} style={{ transitionDelay: '200ms' }}>
-        <MuiCard sx={{ minWidth: 380, maxWidth: 380, marginTop: '6em' }}>
-          <Box sx={{
-            margin: '1em',
-            display: 'flex',
-            justifyContent: 'center',
-            gap: 1
-          }}>
-            <Avatar sx={{ bgcolor: 'primary.main' }}><LockIcon /></Avatar>
-            <Avatar sx={{ bgcolor: 'primary.main' }}><TrelloIcon /></Avatar>
+    <form onSubmit={handleSubmit(submitRegister)}>
+      <Zoom in={true} style={{ transitionDelay: "200ms" }}>
+        <MuiCard sx={{ minWidth: 380, maxWidth: 380, marginTop: "6em" }}>
+          <Box
+            sx={{
+              margin: "1em",
+              display: "flex",
+              justifyContent: "center",
+              gap: 1,
+            }}
+          >
+            <Avatar sx={{ bgcolor: "primary.main" }}>
+              <LockIcon />
+            </Avatar>
+            <Avatar sx={{ bgcolor: "primary.main" }}>
+              <TrelloIcon />
+            </Avatar>
           </Box>
-          <Box sx={{ marginTop: '1em', display: 'flex', justifyContent: 'center', color: theme => theme.palette.grey[500] }}>
-            Author: TrungQuanDev
+          <Box
+            sx={{
+              marginTop: "1em",
+              display: "flex",
+              justifyContent: "center",
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            Author: Cafedev93
           </Box>
-          <Box sx={{ padding: '0 1em 1em 1em' }}>
-            <Box sx={{ marginTop: '1em' }}>
+          <Box sx={{ padding: "0 1em 1em 1em" }}>
+            <Box sx={{ marginTop: "1em" }}>
               <TextField
-                // autoComplete="nope"
+                autoComplete="nope"
                 autoFocus
                 fullWidth
                 label="Enter Email..."
                 type="text"
                 variant="outlined"
+                error={!!errors["email"]}
+                {...register("email", {
+                  required: FIELD_REQUIRED_MESSAGE,
+                  pattern: {
+                    value: EMAIL_RULE,
+                    message: EMAIL_RULE_MESSAGE,
+                  },
+                })}
               />
+              <FieldErrorAlert errors={errors} fieldName={"email"} />
             </Box>
-            <Box sx={{ marginTop: '1em' }}>
+            <Box sx={{ marginTop: "1em" }}>
               <TextField
                 fullWidth
                 label="Enter Password..."
                 type="password"
                 variant="outlined"
+                error={!!errors["password"]}
+                {...register("password", {
+                  required: FIELD_REQUIRED_MESSAGE,
+                  pattern: {
+                    value: PASSWORD_RULE,
+                    message: PASSWORD_RULE_MESSAGE,
+                  },
+                })}
               />
+              <FieldErrorAlert errors={errors} fieldName={"password"} />
             </Box>
-            <Box sx={{ marginTop: '1em' }}>
+            <Box sx={{ marginTop: "1em" }}>
               <TextField
                 fullWidth
                 label="Enter Password Confirmation..."
                 type="password"
                 variant="outlined"
+                error={!!errors["password_confirmation"]}
+                {...register("password_confirmation", {
+                  validate: (value) => {
+                    if (value === watch("password")) return true;
+                    return PASSWORD_CONFIRMATION_MESSAGE;
+                  },
+                })}
+              />
+              <FieldErrorAlert
+                errors={errors}
+                fieldName={"password_confirmation"}
               />
             </Box>
           </Box>
-          <CardActions sx={{ padding: '0 1em 1em 1em' }}>
+          <CardActions sx={{ padding: "0 1em 1em 1em" }}>
             <Button
               type="submit"
               variant="contained"
@@ -68,16 +130,20 @@ function RegisterForm() {
               Register
             </Button>
           </CardActions>
-          <Box sx={{ padding: '0 1em 1em 1em', textAlign: 'center' }}>
+          <Box sx={{ padding: "0 1em 1em 1em", textAlign: "center" }}>
             <Typography>Already have an account?</Typography>
-            <Link to="/login" style={{ textDecoration: 'none' }}>
-              <Typography sx={{ color: 'primary.main', '&:hover': { color: '#ffbb39' } }}>Log in!</Typography>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <Typography
+                sx={{ color: "primary.main", "&:hover": { color: "#ffbb39" } }}
+              >
+                Log in!
+              </Typography>
             </Link>
           </Box>
         </MuiCard>
       </Zoom>
     </form>
-  )
+  );
 }
 
-export default RegisterForm
+export default RegisterForm;
